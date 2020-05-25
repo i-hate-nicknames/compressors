0b35d4c453f6cd3076e805bc25935ba5d7536a96
nvm <nevermn@protonmail.com>
2020-05-25 19:46:31 +0300

    Implement dumping to string output of bitstream

--- str_dump.c
+++ str_dump.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 
-char *itoa(unsigned int val, int base, char buf[33]);
+char *ctoa(unsigned char val, char buf[9]);
 
 int main() {
 
   int c;
   char buf[33];
   while ((c = getchar()) != EOF) {
-    char *bin = itoa(c, 2, buf);
+    char *bin = ctoa(c, buf);
     printf("%s", bin);
   }
   printf("\n");
   return 0;
 }
 
-char *itoa(unsigned int val, int base, char buf[33]) {
-  buf[32] = 0;
-  int i = 31;
-  for (; i && val; --i, val /= base) {
-    buf[i] = "01234567890abcde"[val % base];
+// Convert given value into string representation in binary
+// value, reversed direction.
+// Since the system stores bits right to left, we effectively
+// have "little endianness" in a bit world, but we want
+// to print it in the order bits were put
+char *ctoa(unsigned char val, char buf[9]) {
+  buf[8] = 0;
+  int i;
+  for (i = 0; i < 8; i++, val /= 2) {
+    buf[i] = "01"[val % 2];
   }
-  return &buf[i+1];
+  return buf;
 
 }
